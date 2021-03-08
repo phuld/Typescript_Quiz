a9e45a9e1fdd877907ccf55a9076f34e780e567c
ledphu <ldphu4@gmail.com>
2021-03-08 22:27:26 +0700

    staging2

--- src/API.tsx
+++ src/API.tsx
@@ -8,5 +8,6 @@ export const fetchQuizQuestions = async(amount: number, difficulty: Difficulty) 
     const endpoint=`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
     const data = await(await fetch(endpoint)).json()
     console.log(data);
+    console.log('staging2');
     
 }
